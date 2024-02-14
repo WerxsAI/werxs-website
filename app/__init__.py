d7cbb720a8f42e5efb6ac3cc64737cb4121484dd
wadevondoom <wade.neuwirth@gmail.com>
2024-02-13 20:58:46 -0800

    WEB-50 Bootstrap and jquery are working

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .routes.public import public_bp
+from .routes.admin import admin_hp
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(public_bp)
+    app.register_blueprint(admin_hp)
 
     return app
--- app/routes/admin.py
+++ app/routes/admin.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 admin_hp = Blueprint("admin", __name__)
 
 
-@admin_hp.route("/")
+@admin_hp.route("/admin")
 def home():
-    return render_template("admin_hp.html")
+    return render_template("admin.html")
